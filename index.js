0e1e5e09a62d7be78e450f8a7b73f03f92bf89c0
danigb <danigb@gmail.com>
2015-12-01 11:54:18 +0100

    0.0.1 First version

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 'use strict'
 
 var chords = require('./chords.json')
-var parse = require('tonal.notation/interval.parse')
+var dictionary = require('tonal.dictionary/dictionary')
 
 /**
  * A chord dictionary. Get chord data from a chord name.
  *
- * The chord data object contains:
- * - name: the name of the chord
+ * The chord dictionary is a function that returns a chord data object.
+ * The chord data object has:
+ *
+ * - name: the name
  * - aliases: an array with the alternative names of the chord
  * - intervals: an array with the intervals
  * - steps: an array with the intervals in __array notation__
  * - binary: a binary representation of the chord set
  * - decimal: the decimal representation of the chord set
+ *
+ * The dictionary has a `names` property with all chord names.
  *
  * @name chord.dictionary
  * @function
@@ -29,29 +33,9 @@ var parse = require('tonal.notation/interval.parse')
  * @example
  * // get it from aliases, binary or decimal numbers
  * chord('Maj7') === chord('M7') === chord('100010010001') === chord(2913)
+ *
+ * @example
+ * // get chord names
+ * chord.names // => ['Maj7', 'm7', ...]
  */
 module.exports = dictionary(chords)
-
-function dictionary (src) {
-  function dict (key) { return dict.data[key] }
-  dict.source = src
-  dict.names = Object.keys(src)
-  var data = dict.data = {}
-
-  dict.names.forEach(function (k) {
-    var d = src[k]
-    var c = { name: k, aliases: d[1] || [] }
-    c.intervals = d[0].split(' ')
-    c.steps = c.intervals.map(parse)
-    c.binary = binary([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], c.steps)
-    c.decimal = parseInt(c.binary, 2)
-    data[k] = data[c.binary] = data[c.decimal] = c
-    c.aliases.forEach(function (a) { data[a] = c })
-  })
-  return dict
-}
-
-function binary (num, intervals) {
-  intervals.forEach(function (i) { num[(i[0] * 7 + i[1] * 12) % 12] = '1' })
-  return num.join('')
-}
